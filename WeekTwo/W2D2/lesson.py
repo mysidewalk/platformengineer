08fbe227fc43ca781295aaf21a8cbb6ba19c9894
dfowler <dfowler@mindmixer.com>
2015-01-27 01:10:37 -0600

    cleaned up formatting for headers

--- WeekTwo/W2D2/lesson.py
+++ WeekTwo/W2D2/lesson.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 '''
-MindMixer Platform Engineer Training
-L1:W2D2 - flow control, sequences, and iterables
+MindMixer Platform Engineer Training - Part 1
+W2D2 - flow control, sequences, and iterables
 
 Pro-tip: run a specific function from the command line directly like so:
 python -c 'import this_filename; my_function(a_parameter,b_parameter)'
