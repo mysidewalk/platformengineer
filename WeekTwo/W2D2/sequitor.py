a44a46dde3f407deb8f2b07e24abacafd6ce5215
dfowler <dfowler@mindmixer.com>
2015-01-27 03:07:57 -0600

    added initial sequitor skeleton

---
+++ WeekTwo/W2D2/sequitor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+'''
+MindMixer Platform Engineer Training - Part 1
+W2D2 - flow control, sequences, and iterables
+
+Pro-tip: run a specific function from the command line directly like so:
+python -c 'import this_filename; my_function(a_parameter,b_parameter)'
+(you must be in the directory where "this_filename.py" resides)
+
+'''
+
+# Place any needed module imports here...
