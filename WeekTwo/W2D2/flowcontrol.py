25c8a4d129eb8c6e1a5f980b952039e99a069c13
dfowler <dfowler@mindmixer.com>
2015-01-27 03:04:28 -0600

    added flowcontrol exercise skeleton

---
+++ WeekTwo/W2D2/flowcontrol.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+'''
+MindMixer Platform Engineer Training - Part 1
+W2D2 - flow control, sequences, and iterables
+
+Pro-tip: run a specific function from the command line directly like so:
+python -c 'import this_filename; my_function(a_parameter,b_parameter)'
+(you must be in the directory where "this_filename.py" resides)
+
+'''
+
+# Place any needed module imports here...
+# Example: import modulename as mn
+
+# Please see the "Control Flow Structures Exercises" portion of
+# pairedexercises.md from today's class directory for further information.
+
+def inhabitant_growth():
+    '''
+    Use a for loop to solve the following problem:
+    The country of Swaziland has X inhabitants, and its population
+    grows 3% per year. The nation of Djibouti has a population of Y
+    and grows 2% per year. Return in how many years Swaziland will
+    surpass Djibouti.
+    '''
+
+def hello_timed_world():
+    '''
+    Use a while loop to print "Hello world!" for ~3 seconds.
+    '''
+
+def bad_grades():
+    '''
+    Use 'if'/'elif'/'else' statements to return a letter grade
+    based on a 0-100 integer input. Be sure to allow for at least
+    two significant digits after the decimal point.
+
+    Cut-offs for each letter shall be as follows:
+
+    A = Greater than or equal to 90
+    B = 80 to 89
+    C = 70 to 79
+    D = 60 to 69
+    F = Less than but not equal to 60
+    '''
+
+def main():
+    '''
+    For extra points, modify this main function to call your
+    other completed functions. You may statically define your
+    function parameters
+    '''
