ad36bc8dd3a79e997576f606176615c346cd41cb
dfowler <dfowler@mindmixer.com>
2015-01-29 10:00:59 -0600

    adding classes exercises

---
+++ WeekTwo/W2D4/classes_ex.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+'''
+MindMixer Platform Engineer Training - Part 1
+W2D4 - all about classes 
+
+Exercises related to: classes, attributes and methods
+
+Pro-tip: run a specific function from the command line directly like so:
+python -c 'import this_filename; my_function(a_parameter,b_parameter)'
+(you must be in the directory where "this_filename.py" resides)
+
+'''
+
+# Place any needed module imports here...
+# Example: import modulename as mn
+
+class AttribMethods:
+    '''
+    Complete this class with a class-level attribute,
+    an instance-level attribute and two methods
+    '''
+    def __init__(self):
+        return 0
+
+class NumericString:
+    '''
+    Complete this class so it holds a numeric instance attribute
+    and a string instance attribute. Decide on four operators/magic
+    methods you want to override for this class and provide custom
+    functionalities for these methods based on the data in the class.
+    '''
+    def __init__(self):
+	return 0
+
+
+def main():
+    '''
+    For extra points, modify this main function to call instantiate
+    instances of your completed classes.
+
+    You may statically define the class parameters that are
+    passed in for each instantiation, or utilize a module to
+    grab arbitrary parameters from the command line invocation.
+
+    Either is perfectly valid.
+    '''
+
+
+# Keep this as-is to invoke main.
+if __name__ == '__main__':
+    main()
